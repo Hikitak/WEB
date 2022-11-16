2a551e74f626024af4e34bbe80319223f26c4cc6
Nikita K <cpencipogu@mail.ru>
2022-11-16 20:00:55 +0300

    Попытка исправить

--- LR3/code/get_image.php
+++ LR3/code/get_image.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 if($_SESSION['user']){
-    $file = file_get_contents('img/' . $_GET['img_path'], true);
-    echo $file;
-    //header('Content-type: image/jpeg');
-    //imagejpeg(imagecreatefromjpeg('img/'. $_GET['img_path']));
+    //$file = file_get_contents('../img_pack/' . $_GET['img'], true);
+    //echo $file;
+    header('Content-type: image/jpeg');
+    imagejpeg(imagecreatefromjpeg('img/'. $_GET['img_path']));
 }else{
     header('Location: login.php');
 }
