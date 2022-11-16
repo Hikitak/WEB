667911ff7519b21ba74dcc4c089ed3bdbbed81b9
Nikita K <cpencipogu@mail.ru>
2022-11-16 21:53:35 +0300

    fixed

--- LR4/LR4/text_logic.php
+++ LR4/LR4/text_logic.php
@@ -1,3 +1,4 @@
+
 <?php
 require "presets.php";
 error_reporting(0);
@@ -15,7 +16,7 @@ if ($_GET["preset"]) {
         $text = $preset1;
     }
     elseif ($_GET["preset"] == "2") {
-        $text = $preset2;
+        $text = $preset0;
     }
     elseif ($_GET["preset"] == "3") {
         $text = $preset3;
@@ -55,13 +56,13 @@ else {
                 &&mb_strtolower($elem)!='ни' && mb_strtolower($elem)!='у' && mb_strtolower($elem)!='о'
                 && mb_strtolower($elem)!='к' && mb_strtolower($elem)!='в' && mb_strtolower($elem)!='до'
                 &&mb_strtolower($elem)!='с' && mb_strtolower($elem)!='от' && mb_strtolower($elem)!='об'
-                &&mb_strtolower($elem)!='по' && mb_strtolower($elem)!='под' && mb_strtolower($elem)!='без'
+                &&mb_strtolower($elem)!='по' && mb_strtolower($elem)!='под' && mb_strtolower($elem)!='без' && !is_numeric($elem)
             ){
                 $count = 0;
                 $times[$i] = preg_match_all("/(\b" . $elem . "\b)(?!=<\/r>)/ui", $mainText);
                 print "<div>$i. <a href='#part$i'>$elem</a> - $times[$i] раз</div>";
                 $i++;
-                if ($i == 100)
+                if ($i == 101)
                     break;
             }
 
@@ -69,26 +70,44 @@ else {
 
     }
     //17
-    function highlightRepeatedWords($mainText){
+/*function highlightRepeatedWords($mainText){
 
-        //  $mainText = preg_replace('/(\b[а-я]+\b)(?=.*\1)/ui',"-",$mainText);
-
-        $textArr = preg_match_all('/(\b\w+\b)/ui',$mainText, $matches);
-        $matches[0] = array_unique($matches[0]);
-        foreach($matches[0] as $elem){
-            $count = 0;
-            $mainText= preg_replace_callback("/(\b".$elem."\b)(?!=<\/r>)/ui", function($item) use(&$count){
-                $count++;
-                if($count > 1){
-                    return "<r style='background-color: yellow'>".$item[0]."</r>";
-                }else{
-                    return $item[0];
-                }
-            },$mainText);
-        }
-
-        return $mainText;
+    //  $mainText = preg_replace('/(\b[а-я]+\b)(?=.*\1)/ui',"-",$mainText);
+    $textArr = preg_match_all('/(\b\w+\b)/ui',strip_tags($mainText), $matches);
+    $matches[0] = array_unique($matches[0]);
+    foreach($matches[0] as $elem){
+        $count = 0;
+        $mainText= preg_replace_callback("/(\b".$elem."\b)(?!=<\/r>)/ui", function($item) use(&$count){
+            $count++;
+            if($count > 1){
+                return "<r class='yellowBackground'>".$item[0]."</r>";
+            }else{
+                return $item[0];
+            }
+        },$mainText);
     }
+
+    return $mainText;
+}*/
+function highlightRepeatedWords($text){
+    $cleanedtext = strip_tags($text);
+    $count = array();
+    $words = preg_split('/([^\w+]+|\bно\b|\bв\b|\bесли\b|\bи\b|\bпо\b|\bа\b|\bже\b|\d+|\b\w{1,3}\b)/ui',mb_strtolower($cleanedtext));
+    foreach($words as $word){
+        if(empty($word))
+            continue;
+        if(!isset($count[$word])){
+            $count[$word]=1;continue;
+        }
+        if($count[$word]==1){
+            $text = preg_replace('/\b('.$word.')\b/ui','<my-yellow>'.$word.'</my-yellow>',$text);
+            $text = preg_replace('/<my-yellow>'.$word.'</my-yellow>/ui',$word,$text,1);
+            $count[$word]=2;
+        }
+    }
+    return $text;
+}
+
     //base
     function baseText($mainText){
         $textArr = preg_match_all('/(\b\w+\b)/ui',$mainText, $matches);
@@ -105,7 +124,7 @@ else {
             ) {
                     $mainText=preg_replace("/(\b".$elem."\b)(?!=<\/r>)/ui","<r id='part$i'>$elem</r>",$mainText,1);
                     $i++;
-                    if($i==100)
+                    if($i==101)
                         break;
             }
         }
